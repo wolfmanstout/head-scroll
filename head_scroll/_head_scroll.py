239f79db0f57d82307efcef2b2a00ad5230c3a5f
James Stout <james.wolf.stout@gmail.com>
2021-01-04 18:10:22 -0800

    Fixed concurrency issue.

--- head_scroll/_head_scroll.py
+++ head_scroll/_head_scroll.py
@@ -16,27 +16,34 @@ class Scroller(object):
         self.fast_threshold = fast_threshold
         self.stop_threshold = stop_threshold
         self.scroll_period = scroll_period
+        self._stop_event = None
 
     def start(self):
+        if self._stop_event:
+            return
         start_gaze = self.eye_tracker.get_gaze_point_or_default()
         self.mouse.move((int(start_gaze[0]), int(start_gaze[1])))
-        self._start_rotation = self.eye_tracker.get_head_rotation_or_default()
-        self._last_rotation = self._start_rotation
-        self._stop_event = threading.Event()
-        thread = threading.Thread(target=self._run)
+        stop_event = threading.Event()
+        thread = threading.Thread(target=lambda: self._run(stop_event))
         thread.setDaemon(True)
         thread.start()
+        self._stop_event = stop_event
 
     def stop(self):
+        if not self._stop_event:
+            return
         self._stop_event.set()
+        self._stop_event = None
 
-    def _run(self):
-        while not self._stop_event.is_set():
+    def _run(self, stop_event):
+        start_rotation = self.eye_tracker.get_head_rotation_or_default()
+        last_rotation = start_rotation
+        while not stop_event.is_set():
             time.sleep(self.scroll_period)
 
             rotation = self.eye_tracker.get_head_rotation_or_default()
-            start_delta = rotation[0] - self._start_rotation[0]
-            last_delta = rotation[0] - self._last_rotation[0]
+            start_delta = rotation[0] - start_rotation[0]
+            last_delta = rotation[0] - last_rotation[0]
 
             if start_delta >= self.fast_threshold and last_delta > -self.stop_threshold:
                 self.mouse.scroll_up(4)
@@ -47,4 +54,4 @@ class Scroller(object):
             elif start_delta <= -self.start_threshold and last_delta < self.stop_threshold:
                 self.mouse.scroll_down()
 
-            self._last_rotation = rotation
+            last_rotation = rotation
